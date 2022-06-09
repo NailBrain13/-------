78283de00582562e2b540f14fbbd5b8ac888ae93
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-06-09 19:35:16 +0300

    Fix 'props'

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,21 +2,17 @@ import BtnDelete from '../Buttons/Delete';
 import Count from '../Count';
 import './style.scss';
 
-const Product = (props) => {
-  console.log(props.product);
+const Product = ({ product }) => {
   return (
     <section className='product'>
       <div className='product__img'>
-        <img
-          src={`./img/products/${props.product.img}`}
-          alt={props.product.title}
-        />
+        <img src={`./img/products/${product.img}`} alt={product.title} />
       </div>
-      <div className='product__title'>{props.product.title}</div>
+      <div className='product__title'>{product.title}</div>
       <div className='product__count'>
         <Count />
       </div>
-      <div className='product__price'>{props.product.price} руб</div>
+      <div className='product__price'>{product.price} руб</div>
       <div className='product__controls'>
         <BtnDelete />
       </div>
