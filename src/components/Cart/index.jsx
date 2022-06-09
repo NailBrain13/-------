3daeee0c02a5b01c671b0be46440ef286bd93afe
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-06-09 19:30:58 +0300

    Products update

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,14 +1,20 @@
 import CartFooter from '../CartFooter';
 import CartHeader from '../CartHeader';
 import Product from '../Product';
+import products from '../../data';
+import { useState } from 'react';
 
 const Cart = () => {
+  const [cart, setCart] = useState(products);
+
+  const productItems = cart.map((item) => {
+    return <Product product={item} />;
+  });
+
   return (
     <section className='cart'>
       <CartHeader />
-
-      <Product />
-
+      {productItems}
       <CartFooter />
     </section>
   );
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,51 +2,25 @@ import BtnDelete from '../Buttons/Delete';
 import Count from '../Count';
 import './style.scss';
 
-const Product = () => {
+const Product = (props) => {
+  console.log(props.product);
   return (
-    <>
-      <section className='product'>
-        <div className='product__img'>
-          <img src='./img/products/macbook.jpg' alt='Apple MacBook Air 13' />
-        </div>
-        <div className='product__title'>Apple MacBook Air 13</div>
-        <div className='product__count'>
-          <Count />
-        </div>
-        <div className='product__price'>110 000 руб.</div>
-        <div className='product__controls'>
-          <BtnDelete />
-        </div>
-      </section>
-
-      <section className='product'>
-        <div className='product__img'>
-          <img src='./img/products/apple-watch.jpg' alt='Apple watch' />
-        </div>
-        <div className='product__title'>Apple watch</div>
-        <div className='product__count'>
-          <Count />
-        </div>
-        <div className='product__price'>29 000 руб.</div>
-        <div className='product__controls'>
-          <BtnDelete />
-        </div>
-      </section>
-
-      <section className='product'>
-        <div className='product__img'>
-          <img src='./img/products/mac-pro.jpg' alt='Mac Pro' />
-        </div>
-        <div className='product__title'>Mac Pro</div>
-        <div className='product__count'>
-          <Count />
-        </div>
-        <div className='product__price'>190 000 руб.</div>
-        <div className='product__controls'>
-          <BtnDelete />
-        </div>
-      </section>
-    </>
+    <section className='product'>
+      <div className='product__img'>
+        <img
+          src={`./img/products/${props.product.img}`}
+          alt={props.product.title}
+        />
+      </div>
+      <div className='product__title'>{props.product.title}</div>
+      <div className='product__count'>
+        <Count />
+      </div>
+      <div className='product__price'>{props.product.price} руб</div>
+      <div className='product__controls'>
+        <BtnDelete />
+      </div>
+    </section>
   );
 };
 
