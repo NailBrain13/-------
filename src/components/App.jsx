80c6903c63ebeb2ae92f0e1e64122693d6f5b5ec
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-06-09 17:26:25 +0300

    Add styles

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import './App/_reset.scss';
+import './App/_base.scss';
+import './App/_vars.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React app</h1>
+    </div>
+  );
+}
+
+export default App;
